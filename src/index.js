3d791df521685b1555bc4deb82266564ec415b62
Slumrag <mrgreenthecristal@yandex.ru>
2023-07-21 19:02:31 +0300

    init: start reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    return +number
+        .toString()
+        .split("")
+        .filter((item) => /[0-9]/.test(item))
+        .reverse()
+        .join("");
+};
